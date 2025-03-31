7ddc2ce7b741775a950437cb1d905726d2ba875d
Evening Doggie <04.sunrise_picnics@icloud.com>
2025-04-01 01:04:53 +0300

    Ex2 demonstration code

--- Ex2/src/Main.java
+++ Ex2/src/Main.java
@@ -6,18 +6,20 @@ public class Main
 {
     public static void main(String[] args) throws CloneNotSupportedException
     {
-        var p1 = new Polynomial(3,3,3,3);
-        var p2 = new Polynomial(5,5,5,5);
+        //Input from lowest to highest degrees
 
-        var res = Polynomial.divide(p1, p2);
+        var p1 = new Polynomial(8,8,4,4);
+        var p2 = new Polynomial(2,2,2,2);
 
-        System.out.printf("First polynomial: %s\n", p1);
-        System.out.printf("Second polynomial: %s\n", p2);
-        System.out.printf("Divide result: %s\n", res);
+        System.out.print("Polynomials:\n");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println();
+        System.out.printf("Compare result: %s\n", p1.compareTo(p2));
+        System.out.printf("Addition result: %s\n", Polynomial.addition(p1,p2));
+        System.out.printf("Subtract result: %s\n", Polynomial.subtract(p1,p2));
+        System.out.printf("Multiply result: %s\n", Polynomial.multiply(p1, p2));
+        System.out.printf("Divide result: %s\n", Polynomial.divide(p1, p2));
 
-        //System.out.println(result._result);
-       // System.out.println(result._quotient);
-
-        System.out.printf("Compare result: %s", p1.compareTo(p2));
     }
 }
