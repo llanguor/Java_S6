0e2bb6bfc902a996ec5b9dc89aa69a89803c3030
Evening Doggie <04.sunrise_picnics@icloud.com>
2025-03-31 23:22:52 +0300

    Ex3

--- Ex3/src/Main.java
+++ Ex3/src/Main.java
@@ -1,3 +1,4 @@
+import Packages.Matrix;
 import java.util.*;
 
 public class Main
@@ -5,100 +6,38 @@ public class Main
 
     public static void main(String[] args)
     {
-        //Число не всегда максимальное.
-        //Иначе алгоритм запирал бы сам себя
-        //А по условию нужно обязательно обойти ВСЕ ячейки
-        
-        int[][] matrix = Matrix.randomizeMatrix();
-        Matrix.print(matrix);
-        System.out.println("Max number: " + Matrix.findMaxNumber(matrix, 3, 3));
+        String input = null;
+        Scanner in = new Scanner(System.in);
+        int[][] matrix = {};
 
-    }
-
-    public static final class Matrix
-    {
-
-        public static void print(int[][] matrix)
+        while (true)
         {
-            for (int[] row : matrix)
+            System.out.print("\nEnter numbers in array (r - random generation, e - exit)\n");
+            input = in.nextLine();
+
+            switch (input)
             {
-                for (int num : row)
-                {
-                    System.out.print(num + " ");
-                }
-                System.out.println();
-            }
-        }
+                case "e":
+                    return;
 
-        public static int[][] randomizeMatrix()
-        {
+                case "r":
+                    matrix = Matrix.randomize(3,3);
+                    break;
 
-            List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
-            Collections.shuffle(numbers);
-
-            int[][] matrix = new int[3][3];
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    matrix[i][j] = numbers.get(i * 3 + j);
-                }
-            }
-            return matrix;
-        }
-
-
-        private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-        public static int findMaxNumber(int[][] matrix, int rows, int cols) {
-
-            for (int i = 0; i < rows; ++i)
-            {
-                for (int j = 0; j < cols; ++j)
-                {
-                    if (matrix[i][j] == 9)
+                default:
+                    try
                     {
-                        return depthFirstSearch(
-                                matrix,
-                                i,
-                                j,
-                                new HashSet<>(),
-                                matrix[i][j],
-                                0);
+                        matrix = Matrix.initFromString(input, 3, 3);
                     }
-                }
+                    catch (IllegalArgumentException e)
+                    {
+                        System.err.println(e.getMessage());
+                        continue;
+                    }
             }
 
-            throw new IllegalArgumentException("The input array is invalid. It must contain digits from 1 to 9");
-        }
-
-        private static int depthFirstSearch(int[][] matrix, int x, int y, Set<String> visited, int number, int maxNumber)
-        {
-            maxNumber = Math.max(maxNumber, number);
-            visited.add(x + "," + y);
-
-            //Перебор направлений
-            for (int[] dir : DIRECTIONS)
-            {
-                //Получаем новые индексы.
-                int nx = x + dir[0], ny = y + dir[1];
-
-                //Переходим к след направлению если какой-то индекс вне массива или ячейка уже посещена
-                if(nx<0 || nx>2 || ny<0 || ny>2 || visited.contains(nx + "," + ny))
-                    continue;
-
-                //Продолжаем рекурсивно идти по ячейкам, пока не обойдём все в каждом рекурсивном вызове
-                maxNumber = depthFirstSearch(
-                        matrix,
-                        nx,
-                        ny,
-                        new HashSet<>(visited),
-                        number*10 + matrix[nx][ny],
-                        maxNumber);
-
-            }
-
-            return maxNumber;
+            Matrix.print(matrix);
+            System.out.println("Max number: " + Matrix.MaxNumberFinder.find(matrix));
         }
     }
 }
---
+++ Ex3/src/Packages/Matrix.java
@@ -0,0 +1,120 @@
+package Packages;
+import java.util.*;
+
+public final class Matrix
+{
+    public static void print(int[][] matrix)
+    {
+        for (int[] row : matrix)
+        {
+            for (int num : row)
+            {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] randomize(int rows, int cols)
+    {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=0; i<rows*cols; ++i)
+        {
+            numbers.add(i+1);
+        }
+        Collections.shuffle(numbers);
+
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i][j] = numbers.get(i * rows + j);
+            }
+        }
+        return matrix;
+    }
+
+    public static int[][] initFromString(String input, int rows, int cols)
+    {
+        if(!input.matches("\\d+"))
+            throw new IllegalArgumentException("Incorrect input. Values must be numbers");
+
+        if (input.length() != rows * cols)
+            throw new IllegalArgumentException("Incorrect length of input data");
+
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0, index = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i][j] = Character.getNumericValue(
+                        input.charAt(index++));
+            }
+        }
+
+        return matrix;
+    }
+
+    public static final class MaxNumberFinder
+    {
+        private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        public static int find(int[][] matrix)
+        {
+            //Начинаем с 1 из 9 ячеек и выбираем большее число
+
+            int rows = matrix.length;
+            int cols = matrix[0].length;
+            int maxNumber = 0;
+
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < cols; ++j)
+                {
+                    //Не можем начинать с ячеек, где сумма индексов нечетно,
+                    //если и число стобцов и число строк нечетно
+                    //Упрёмся в стенку, тк не хватит "ширины"
+
+                    if(rows%2==1 && cols%2==1 && (i+j)%2==1)
+                        continue;
+
+                    int tempNumber = depthFirstSearch(
+                            matrix,
+                            i,
+                            j,
+                            new HashSet<>(),
+                            matrix[i][j],
+                            0);
+
+                    maxNumber = Math.max(tempNumber, maxNumber);
+                }
+            }
+
+            return maxNumber;
+        }
+
+        private static int depthFirstSearch(int[][] matrix, int i, int j, Set<Integer> visited, int number, int maxNumber)
+        {
+            maxNumber = Math.max(maxNumber, number);
+            visited.add(i*10+j);
+
+            for (int[] dir : DIRECTIONS)
+            {
+                int ni = i + dir[0], nj = j + dir[1];
+                if (ni < 0 || ni > 2 || nj < 0 || nj > 2 || visited.contains(ni*10+nj))
+                    continue;
+
+                maxNumber = depthFirstSearch(
+                        matrix,
+                        ni,
+                        nj,
+                        new HashSet<>(visited),
+                        number*10 + matrix[ni][nj],
+                        maxNumber);
+
+            }
+            return maxNumber;
+        }
+    }
+}
